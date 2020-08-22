6364b31c18ceb3defbe6639d9d96a9cd1ff5b04e
John Sigsworth <john.a.sigsworth@gmail.com>
2020-08-22 16:44:39 +0100

    Updates

---
+++ precache-manifest.e4fddb2eaba618641bbf40f4281d124b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fcf43f0958a03a3b033c5f736ffc560",
+    "url": "/jasabc.github.io/index.html"
+  },
+  {
+    "revision": "22a61a9a0c1370cf956d",
+    "url": "/jasabc.github.io/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1eabcf1a836653ad63f4",
+    "url": "/jasabc.github.io/static/js/2.3a4a000d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jasabc.github.io/static/js/2.3a4a000d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22a61a9a0c1370cf956d",
+    "url": "/jasabc.github.io/static/js/main.4e945710.chunk.js"
+  },
+  {
+    "revision": "15917fa5bb5b27872597",
+    "url": "/jasabc.github.io/static/js/runtime-main.45fba1dd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/jasabc.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
